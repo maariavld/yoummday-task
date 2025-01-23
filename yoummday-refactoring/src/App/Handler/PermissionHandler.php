efe951e5df0b0e52d6d8e5dd06f5ee31f8e2627e
mvlad <maaria.vld@gmail.com>
2025-01-23 10:05:28 +0100

    **Removed `MonologLogger.php`:**
    Monolog library is now used directly in the handler.  It would be ideal to use DI to inject logger as it should normally call the same instance in every file. I've had issues with registering the Monolog/Logger with the Injector class as it considered the Logger class a primitive and not a class to be registered.
    
    **Changes in `PermissionHandler.php`:**
    
    * **Direct Monolog Usage:** The `PermissionHandler` now directly uses `Monolog\Logger`
    * **Dependency Injection and Logger Setup:** The constructor no longer takes the `Logger` as a dependency.  Instead, a `setLogger` method annotated with `#[Required]` has been added. The logger is now injected *after* object creation. The `setLogger` method also configures the logger with a `StreamHandler` targeting `php://stdout` with the `Debug` log level. This ensures all logs, including debug messages, are written to standard output.
    * **Consistent Error Handling:** Returning a 403 (Forbidden) for permission issues rather than a 400(Bad Request), 404 (Not Found) for better security.

--- yoummday-refactoring/src/App/Handler/PermissionHandler.php
+++ yoummday-refactoring/src/App/Handler/PermissionHandler.php
@@ -7,6 +7,8 @@ namespace App\Handler;
 use App\Provider\TokenDataProvider;
 use App\Utils\PermissionConst;
 use App\Utils\StatusCodeConst;
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
 use Monolog\Logger;
 use ProgPhil1337\SimpleReactApp\HTTP\Routing\Attribute\Route;
 use ProgPhil1337\SimpleReactApp\HTTP\Routing\Handler\HandlerInterface;
@@ -15,31 +17,31 @@ use ProgPhil1337\SimpleReactApp\HTTP\Routing\RouteParameters;
 use Psr\Http\Message\ServerRequestInterface;
 use ProgPhil1337\SimpleReactApp\HTTP\Response\ResponseInterface;
 use ProgPhil1337\SimpleReactApp\HTTP\Response\JSONResponse;
+use Symfony\Contracts\Service\Attribute\Required;
 
 #[Route(httpMethod: HttpMethod::GET, uri: '/has_permission/{token}')]
 class PermissionHandler implements HandlerInterface
 {
+    private Logger $logger;
     /**
      * Dependency Injection would be available here
      */
     public function __construct(
         protected TokenDataProvider $tokenDataProvider,
-        protected Logger $logger
-    ) {}
+    ) {
+    }
 
     public function __invoke(ServerRequestInterface $serverRequest, RouteParameters $parameters): ResponseInterface
     {
+        $this->setLogger(new Logger('app'));
+
         $requestToken = $parameters->get("token");
         $this->logger->info("Checking permission for token: $requestToken");
-
-        if(!$requestToken) {
-            return new JSONResponse(["permission" => false], StatusCodeConst::FORBIDDEN);
-        }
 
         $tokenData = $this->findToken($requestToken);
 
         if (!$tokenData) {
-            return new JSONResponse(['permission' => false], StatusCodeConst::NOT_FOUND);
+            return new JSONResponse(['permission' => false], StatusCodeConst::FORBIDDEN);
         }
 
         $hasPermission = in_array(PermissionConst::READ, $tokenData['permissions'], true);
@@ -53,7 +55,6 @@ class PermissionHandler implements HandlerInterface
     private function findToken($requestToken): ?array
     {
         $tokens = $this->tokenDataProvider->getTokens();
-
         foreach ($tokens as $t) {
             if ($t["token"] == $requestToken) {
                 return $t;
@@ -62,4 +63,12 @@ class PermissionHandler implements HandlerInterface
 
         return null;
     }
+
+    #[Required]
+    public function setLogger(Logger $logger): void
+    {
+        $this->logger = $logger;
+        $this->logger->pushHandler(new StreamHandler('php://stdout', Level::Debug));
+    }
+
 }
